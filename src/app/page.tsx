c8befcbbe2f8d0fe87851803ca962247d53c934e
Matt Seidholz <mseidholz@gmail.com>
2023-07-09 09:32:34 -0400

    set up basic fetch call

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from 'next/image';
+import styles from './page.module.css';
 
-export default function Home() {
+async function getData() {
+  const res = await fetch('https://smell-the-roses.onrender.com/plants');
+
+  console.log({ res });
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+
+  return res;
+}
+
+export default async function Home() {
+  const data = await getData();
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -91,5 +109,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
